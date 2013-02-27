14106b37f2478485a504503d6c0389a825286c31
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-02-27 09:57:42 +0900

    create db file and logging

--- lib/fluent/plugin/out_sqlite3.rb
+++ lib/fluent/plugin/out_sqlite3.rb
@@ -1,30 +1,41 @@
+require "sqlite3"
+
 class Fluent::Sqlite3Output < Fluent::BufferedOutput
   Fluent::Plugin.register_output('sqlite3', self)
 
   config_param :path, :string
 
   def initialize
-    $log.debug "initialized"
+    super
   end
 
   def configure(conf)
     super
+    @path = conf['path']
   end
   
   def start
     super
+    $log.debug "path: ", @path
+    @db = ::SQLite3::Database.new @path
   end
   
   def shutdown
     super
+    $log.debug "shutdown"
   end
 
   def format(tag, time, record)
+    $log.debug "tag: ", tag
+    $log.debug "time: ", time
+    $log.debug "record: ", record
   end
 
   def client
+    $log.debug "client"
   end
 
   def write(chunk)
+    $log.debug "chunk: ", chunk
   end
 end
